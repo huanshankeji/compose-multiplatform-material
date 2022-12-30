ebc3db49319e387a9de020b390a3a372f2cb15ec
Shreck Ye <ShreckYe@gmail.com>
2022-12-30 22:21:13 +0800

    Bump Compose to 1.2.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin", "1.7.20"))
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.2.1")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.2.2")
     implementation("com.huanshankeji.team:gradle-plugins:0.3.2") {
         exclude("org.jetbrains.kotlin")
     }
