c7adf748b3273373d5de83a9f978758dcd926e36
Shreck Ye <ShreckYe@gmail.com>
2024-04-18 11:36:58 +0800

    Add the modifier parameter to `RadioButton`

--- compose-multiplatform-material/src/androidxCommonMain/kotlin/com/huanshankeji/compose/material/RadioButton.kt
+++ compose-multiplatform-material/src/androidxCommonMain/kotlin/com/huanshankeji/compose/material/RadioButton.kt
@@ -1,7 +1,8 @@
 package com.huanshankeji.compose.material
 
 import androidx.compose.runtime.Composable
+import com.huanshankeji.compose.ui.Modifier
 
 @Composable
-actual fun RadioButton(selected: Boolean, onClick: (() -> Unit)?, enabled: Boolean) =
+actual fun RadioButton(selected: Boolean, onClick: (() -> Unit)?, modifier: Modifier, enabled: Boolean) =
     androidx.compose.material.RadioButton(selected, onClick, enabled = enabled)
--- compose-multiplatform-material/src/commonMain/kotlin/com/huanshankeji/compose/material/RadioButton.kt
+++ compose-multiplatform-material/src/commonMain/kotlin/com/huanshankeji/compose/material/RadioButton.kt
@@ -1,8 +1,13 @@
 package com.huanshankeji.compose.material
 
 import androidx.compose.runtime.Composable
+import com.huanshankeji.compose.ui.Modifier
 
-// TODO modifier
 // TODO colors
 @Composable
-expect fun RadioButton(selected: Boolean, onClick: (() -> Unit)?, enabled: Boolean = true)
+expect fun RadioButton(
+    selected: Boolean,
+    onClick: (() -> Unit)?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
+)
