d89775b8c79b0e840eb22b093985297cd6806611
Shreck Ye <ShreckYe@gmail.com>
2024-04-18 21:58:11 +0800

    Add the `Checkbox` composable

---
+++ compose-multiplatform-material/src/jsMain/kotlin/com/huanshankeji/compose/material/Checkbox.js.kt
@@ -0,0 +1,17 @@
+package com.huanshankeji.compose.material
+
+import androidx.compose.runtime.Composable
+import com.huanshankeji.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.toAttrs
+import dev.petuska.kmdc.checkbox.MDCCheckbox
+
+@Composable
+actual fun Checkbox(
+    checked: Boolean,
+    onCheckedChange: ((Boolean) -> Unit)?,
+    modifier: Modifier,
+    enabled: Boolean
+) =
+    MDCCheckbox(checked, !enabled, attrs = modifier.platformModifier.toAttrs {
+        onCheckedChange?.let { onInput { it(!checked) } }
+    })
