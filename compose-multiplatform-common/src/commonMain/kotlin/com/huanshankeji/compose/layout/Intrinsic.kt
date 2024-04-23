9751ee96b7fec2213d658af23ea765f2ea291d14
Shreck Ye <ShreckYe@gmail.com>
2024-04-24 06:44:05 +0800

    Add `IntrinsicSize` and related modifiers that are not tested yet

---
+++ compose-multiplatform-common/src/commonMain/kotlin/com/huanshankeji/compose/layout/Intrinsic.kt
@@ -0,0 +1,12 @@
+package com.huanshankeji.compose.layout
+
+import androidx.compose.runtime.Stable
+import com.huanshankeji.compose.ui.Modifier
+
+@Stable
+expect fun Modifier.width(intrinsicSize: IntrinsicSize): Modifier
+
+@Stable
+expect fun Modifier.height(intrinsicSize: IntrinsicSize): Modifier
+
+enum class IntrinsicSize { Min, Max }
