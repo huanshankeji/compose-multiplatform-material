eb1ada7d629f3aa8831df4588653d87530e2635b
Shreck Ye <ShreckYe@gmail.com>
2024-04-24 16:03:18 +0800

    Bump dependency versions

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -1,14 +1,13 @@
 val projectVersion = "0.2.0-SNAPSHOT"
 
 object DependencyVersions {
-    val composeMultiplatform = "1.6.1" // manually specified for "ui-unit"
-    val kobweb = "0.17.1"
+    val composeMultiplatform = "1.6.2" // manually specified for "ui-unit"
+    val kobweb = "0.17.3"
     val huanshankejiComposeWeb = "0.2.2"
     val kmdc = "0.1.2"
-    val materialIcons = "1.13.12"
 
     object Androidx {
-        val activityCompose = "1.8.2"
-        val compose = "1.6.4"
+        val activityCompose = "1.9.0"
+        val compose = "1.6.6"
     }
 }
