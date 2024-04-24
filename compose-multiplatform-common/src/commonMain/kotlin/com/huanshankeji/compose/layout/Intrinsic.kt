2daac25c712e49a565094e6d4bb4bf56822cc468
Shreck Ye <ShreckYe@gmail.com>
2024-04-24 09:35:05 +0800

    Revert "Temporarily deprecate the code added in commit 9751ee96b7fec2213d658af23ea765f2ea291d14"
    This reverts commit 545180238991177783537d5213248269011709a6.

--- compose-multiplatform-common/src/commonMain/kotlin/com/huanshankeji/compose/layout/Intrinsic.kt
+++ compose-multiplatform-common/src/commonMain/kotlin/com/huanshankeji/compose/layout/Intrinsic.kt
@@ -3,16 +3,10 @@ package com.huanshankeji.compose.layout
 import androidx.compose.runtime.Stable
 import com.huanshankeji.compose.ui.Modifier
 
-private const val DEPRECATION_MESSAGE =
-    "This API doesn't work as expected, is not consistent on both platforms, and is temporarily deprecated."
-
-@Deprecated(DEPRECATION_MESSAGE)
 @Stable
 expect fun Modifier.width(intrinsicSize: IntrinsicSize): Modifier
 
-@Deprecated(DEPRECATION_MESSAGE)
 @Stable
 expect fun Modifier.height(intrinsicSize: IntrinsicSize): Modifier
 
-@Deprecated(DEPRECATION_MESSAGE)
 enum class IntrinsicSize { Min, Max }
