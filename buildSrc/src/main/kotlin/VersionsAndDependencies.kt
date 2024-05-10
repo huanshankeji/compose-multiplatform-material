3487e71606b07c757d1d063a48ca5f5d052b4e9c
Shreck Ye <ShreckYe@gmail.com>
2024-05-10 12:12:23 +0800

    Put the version of "material-symbols" in `DependencyVersions`

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -7,6 +7,7 @@ object DependencyVersions {
     val kobweb = "0.17.3"
     val huanshankejiComposeHtml = "0.3.0"
     val kmdc = "0.1.2"
+    val materialSymbols = "0.17.4"
 
     object Androidx {
         val activityCompose = "1.9.0"
--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -77,7 +77,7 @@ kotlin {
         jsMain {
             dependencies {
                 implementation(compose.html.core)
-                implementation(npm("material-symbols", "0.17.4"))
+                implementation(npm("material-symbols", DependencyVersions.materialSymbols))
             }
         }
     }
