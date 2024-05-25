fe847b24a23f56cfc6f5bed3558de9a6277f70db
Shreck Ye <ShreckYe@gmail.com>
2024-05-25 17:04:17 +0800

    Bump Kobweb to 0.18.0

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -7,7 +7,7 @@ val commonDependencies = CommonDependencies()
 
 object DependencyVersions {
     const val composeMultiplatform = ComposeBuildConfig.composeVersion // for "ui-unit"
-    val kobweb = "0.17.3"
+    val kobweb = "0.18.0"
     val huanshankejiComposeHtml = "0.4.0-SNAPSHOT" // TODO don't use a snapshot version in a main branch
     val kmdc = "0.1.2"
     val materialSymbols = "0.17.4"
