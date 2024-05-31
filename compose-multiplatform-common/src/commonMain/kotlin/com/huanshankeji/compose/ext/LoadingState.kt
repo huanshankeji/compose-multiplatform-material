5685ab41b726413548c65c615c4acbadd900347c
Shreck Ye <ShreckYe@gmail.com>
2024-05-31 09:39:19 +0800

    Move `LoadingState` here from Compose HTML Material
    The corresponding commit: https://github.com/huanshankeji/compose-html-material/commit/5eec3fe5a26ff1ac5bbf3408503994ce51db7394

---
+++ compose-multiplatform-common/src/commonMain/kotlin/com/huanshankeji/compose/ext/LoadingState.kt
@@ -0,0 +1,13 @@
+package com.huanshankeji.compose.ext
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+sealed class LoadingState<out T, out E> {
+    object Loading : LoadingState<Nothing, Nothing>()
+    class Loaded<out T>(val value: T) : LoadingState<T, Nothing>()
+    class Error<out E>(val error: E) : LoadingState<Nothing, E>()
+}
+
+typealias ThrowableErrorLoadingState<T> = LoadingState<T, Throwable>
+typealias StringErrorLoadingState<T> = LoadingState<T, String>
