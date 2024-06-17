f2fefce99e68e67ec466754432c14f9a0c8bb437
Shreck Ye <ShreckYe@gmail.com>
2024-06-17 09:51:27 +0800

    Copy the Gradle build script ios configuration to "destructured-state-in-navhost-composable-bug-demo/iosApp" because build failed

--- destructured-state-in-navhost-composable-bug-demo/build.gradle.kts
+++ destructured-state-in-navhost-composable-bug-demo/build.gradle.kts
@@ -7,6 +7,17 @@ plugins {
 kotlin {
     val outputFileName = "app.js"
 
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         browser {
