0319ad5693c7c544eafb9aeebb7e8a0c2745951b
Shreck Ye <ShreckYe@gmail.com>
2024-07-27 10:06:22 +0800

    Bump Compose to 1.6.11 and manually experiment with "destructured-state-in-navhost-composable-bug-demo" on iOS

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     val kotlinVersion = "2.0.0"
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
-    implementation("org.jetbrains.compose:compose-gradle-plugin:1.6.10")
+    implementation("org.jetbrains.compose:compose-gradle-plugin:1.6.11")
     implementation("com.huanshankeji.team:gradle-plugins:0.5.1")
     implementation("com.android.tools.build:gradle:8.2.2")
     implementation("com.huanshankeji:common-gradle-dependencies:0.7.1-20240516")
