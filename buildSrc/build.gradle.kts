93bdf70446309b6a7c61deed065ad6b51d562174
Shreck Ye <ShreckYe@gmail.com>
2024-09-03 08:18:18 +0800

    Bump AGP to 8.5.2 with the IntelliJ IDEA AGP Upgrade Assistant
    The demo runs with no problem on JVM desktop, JS DOM, Android, and Wasm as tested manually.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,6 +16,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.compose:compose-gradle-plugin:1.6.11")
     implementation("com.huanshankeji.team:gradle-plugins:0.5.1")
-    implementation("com.android.tools.build:gradle:8.2.2")
+    implementation("com.android.tools.build:gradle:8.5.2")
     implementation("com.huanshankeji:common-gradle-dependencies:0.7.1-20240516")
 }
