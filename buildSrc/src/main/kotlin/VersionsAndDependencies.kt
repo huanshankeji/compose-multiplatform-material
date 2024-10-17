0f0b9358efd2c21a03b1fe0c4f0d3c43c7abc6fe
Shreck Ye <ShreckYe@gmail.com>
2024-10-17 21:36:40 +0800

    Bump `DependencyVersions`, run `kotlinUpgradeYarnLock`, and manually ensure that the demo app runs on JVM desktop, JS DOM, and Android
    The app crashes on a "Small Phone" emulator because the screen is not wide enough.
    
    The exception on Android:
    
    ```text
    java.lang.IllegalArgumentException: maxWidth(-104) must be >= than minWidth(0)
    ```
    
    The exception on JVM desktop when the window is narrowed:
    
    ```text
    Exception in thread "AWT-EventQueue-0" java.lang.IllegalArgumentException: maxWidth(-21) must be >= than minWidth(0)
    ```

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -7,15 +7,15 @@ val commonDependencies = CommonDependencies()
 
 object DependencyVersions {
     const val composeMultiplatform = ComposeBuildConfig.composeVersion // for "ui-unit"
-    val kobweb = "0.18.0"
+    val kobweb = "0.19.2"
     val huanshankejiComposeHtml = "0.4.0-SNAPSHOT" // TODO don't use a snapshot version in a main branch
     val kmdc = "0.1.2"
-    val materialSymbols = "0.19.0"
+    val materialSymbols = "0.25.1"
     val composeMultiplatformNavigation = "2.8.0-alpha10"
     val composeMultiplatformLifecycleViewmodel = "2.8.3"
 
     object Androidx {
-        val activityCompose = "1.9.2"
-        val compose = "1.6.7"
+        val activityCompose = "1.9.3"
+        val compose = "1.7.4"
     }
 }
