0d109d29a2851e645eef1206a5d557226870c1ad
Shreck Ye <ShreckYe@gmail.com>
2024-11-08 11:12:09 +0800

    Apply the binary compatibility validator Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
+
+plugins {
+    id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.3"
+}
+
+apiValidation {
+    @OptIn(kotlinx.validation.ExperimentalBCVApi::class)
+    klib {
+        enabled = true
+    }
+}
