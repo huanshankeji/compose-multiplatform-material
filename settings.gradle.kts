5a8aed556366df3f5e50826e866f5057a2cba548
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-11-30 19:05:05 +0800

    Reference a corresponding issue in a TODO

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "compose-multiplatform-html-unified"
 
-include("common") // TODO consider splitting into several modules including `foundation`, `ui`, etc.
+include("common") // TODO consider splitting into several modules including `foundation`, `ui`, etc. (#45)
 include("common:legacy")
 include("material-icons-core")
 include("material2")
