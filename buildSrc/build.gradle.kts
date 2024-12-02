79bdc168b40036ca8cabb4035bc997937362e8fd
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-02 18:49:58 +0800

    Bump our dependencies the latest snapshots using Kotlin 2.1.0, and the project version to be specific about Kotlin 2.1.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,11 +11,11 @@ repositories {
 
 dependencies {
     // With Kotlin 2.0.20, a "Could not parse POM" build error occurs in the JVM projects of some dependent projects.
-    val kotlinVersion = "2.0.10"
+    val kotlinVersion = "2.1.0"
     implementation(kotlin("gradle-plugin", kotlinVersion))
     implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:$kotlinVersion")
     implementation("org.jetbrains.compose:compose-gradle-plugin:1.7.0")
-    implementation("com.huanshankeji.team:gradle-plugins:0.6.0") // don't use a snapshot version in a main branch
+    implementation("com.huanshankeji.team:gradle-plugins:0.7.1-kotlin-2.1.0-SNAPSHOT") // TODO don't use a snapshot version in a main branch
     implementation("com.android.tools.build:gradle:8.5.2")
     implementation("com.huanshankeji:common-gradle-dependencies:0.8.0-20241016") // don't use a snapshot version in a main branch
 }
