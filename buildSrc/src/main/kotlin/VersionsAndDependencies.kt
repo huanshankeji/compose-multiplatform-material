23b4cf1d368866f9f0eefb4c1996493bcbd9d78e
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-04 02:35:48 +0800

    Bump our Gradle plugins to the latest stable version and remove the explicit "kotlin-2.1.0" in the library and project versions

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -1,14 +1,14 @@
 import com.huanshankeji.CommonDependencies
 import org.jetbrains.compose.ComposeBuildConfig
 
-val projectVersion = "0.5.0-kotlin-2.1.0-SNAPSHOT"
+val projectVersion = "0.5.0-SNAPSHOT"
 
 val commonDependencies = CommonDependencies()
 
 object DependencyVersions {
     const val composeMultiplatform = ComposeBuildConfig.composeVersion // for "ui-unit"
     val kobweb = "0.19.2"
-    val huanshankejiComposeHtml = "0.4.1-kotlin-2.1.0-SNAPSHOT" // TODO don't use a snapshot version in a main branch
+    val huanshankejiComposeHtml = "0.4.1-SNAPSHOT" // TODO don't use a snapshot version in a main branch
     val kmdc = "0.1.2"
     val materialSymbols = "0.25.1"
 }
